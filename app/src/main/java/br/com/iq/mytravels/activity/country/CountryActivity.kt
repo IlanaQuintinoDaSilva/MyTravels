a5663ba395b2b1d3d3d1af4e9c776f1594166797
Ilana Quintino da Silva <ilanaquintinodasilva@gmail.com>
2020-06-26 12:39:44 -0300

    Addition of message "Swipe right to see the menu.

--- app/src/main/java/br/com/iq/mytravels/activity/country/CountryActivity.kt
+++ app/src/main/java/br/com/iq/mytravels/activity/country/CountryActivity.kt
@@ -2,6 +2,8 @@ package br.com.iq.mytravels.activity.country
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
 import br.com.iq.mytravels.MyTravelsApplication
 import br.com.iq.mytravels.R
 import br.com.iq.mytravels.data.DatabaseHelper
@@ -11,12 +13,57 @@ import br.com.iq.mytravels.fragment.CountryListFragment
 
 class CountryActivity : AppCompatActivity() {
 
+    private lateinit var mDrawerLayout: DrawerLayout
+    private var country: String = ""
     private var service = CountryService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_country)
         feedInitialData()
+
+        setContentView(R.layout.activity_country)
+
+        mDrawerLayout = findViewById(R.id.drawer_layout_country)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+
+            when (menuItem.itemId) {
+                R.id.nav_travels ->{
+                    if(savedInstanceState == null){
+                        /*setToolBarTitle(getString(R.string.actual_sprint))
+                        addFragment(R.id.container, ToDoListFragment())*/
+                    }
+                }
+                R.id.nav_countries ->{
+                    if(savedInstanceState == null){
+                        addFragment(R.id.container, CountryListFragment())
+                    }
+                }
+                R.id.nav_cities ->{
+                    /*val intent = Intent(context, CategoryActivity::class.java)
+                    startActivity(intent)*/
+                }
+                R.id.nav_places ->{
+                    /*val intent = Intent(context, CloudBackup::class.java)
+                    startActivity(intent)*/
+                }
+                R.id.nav_backup ->{
+
+                }
+            }
+
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+
         addFragment(R.id.container, CountryListFragment())
     }
 
